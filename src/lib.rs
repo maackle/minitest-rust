0cd8a471af5496f8bb627f0553affd7c1a394a50
Michael Dougherty <maackle.d@gmail.com>
2022-08-05 17:36:41 -0700

    Use identity function instead of omitting token

--- src/lib.rs
+++ src/lib.rs
@@ -29,15 +29,15 @@ pub fn minitest(attrs: TokenStream, input: TokenStream) -> TokenStream {
         .to_string();
     let real_type = real_type.expect("minitest must have two arguments");
 
-    fn replace(ts: TokenStream, special: String, replacement: Option<TokenTree>) -> TokenStream {
+    fn replace(ts: TokenStream, special: String, replacement: TokenTree) -> TokenStream {
         ts.into_iter()
-            .flat_map(|tt| match tt {
+            .map(|tt| match tt {
                 TokenTree::Ident(ref i) if i.to_string() == special => replacement.clone(),
-                TokenTree::Group(g) => Some(TokenTree::Group(Group::new(
+                TokenTree::Group(g) => TokenTree::Group(Group::new(
                     g.delimiter(),
                     replace(g.stream(), special.clone(), replacement.clone()),
-                ))),
-                other => Some(other),
+                )),
+                other => other,
             })
             .collect()
     }
@@ -47,10 +47,11 @@ pub fn minitest(attrs: TokenStream, input: TokenStream) -> TokenStream {
     if let Item::Fn(f) = input {
         let mut fa = f.clone();
         let mut fb = f.clone();
+        let id = quote::quote!(std::convert::identity).into();
         let test_body = replace(
             f.block.clone().into_token_stream().into(),
             special.clone(),
-            None,
+            TokenTree::Group(Group::new(Delimiter::Parenthesis, id)),
         );
         let newname = Group::new(
             Delimiter::Parenthesis,
@@ -66,12 +67,7 @@ pub fn minitest(attrs: TokenStream, input: TokenStream) -> TokenStream {
         let real_body = replace(
             f.block.into_token_stream().into(),
             special,
-            Some(
-                TokenTree::Group(newname), // TokenTree::Ident(Ident::new(
-                                           //     &format!("{}::from", real_type.to_string()),
-                                           //     real_type.span(),
-                                           // )),
-            ),
+            TokenTree::Group(newname),
         );
         let ident = f.sig.ident;
         fa.sig.ident = syn::Ident::new(&format!("{}_test", ident.to_string()), ident.span());
@@ -92,71 +88,4 @@ pub fn minitest(attrs: TokenStream, input: TokenStream) -> TokenStream {
     } else {
         panic!("mulitest must be applied to a function");
     }
-    // .into()
-    // let (ident, variants) = match &input {
-    //     Item::Enum(ItemEnum {
-    //         ident, variants, ..
-    //     }) => (ident, variants),
-    //     _ => abort!(input, "hdk_entry_def_conversions can only be used on Enums"),
-    // };
-
-    // let inner: proc_macro2::TokenStream = variants
-    //     .into_iter()
-    //     .map(
-    //         |syn::Variant {
-    //              ident: v_ident,
-    //              fields,
-    //              ..
-    //          }| {
-    //             get_single_tuple_variant(v_ident, fields);
-    //             quote::quote! {#ident::#v_ident (v) => SerializedBytes::try_from(v),}
-    //         },
-    //     )
-    //     .collect();
-    // let try_from_sb: proc_macro2::TokenStream = quote::quote! {
-    //     let result = match t {
-    //         #inner
-    //     };
-    // };
-
-    // let output = quote::quote! {
-    //     #input
-
-    //     impl TryFrom<&#ident> for AppEntryBytes {
-    //         type Error = WasmError;
-    //         fn try_from(t: &#ident) -> Result<Self, Self::Error> {
-    //             #try_from_sb
-    //             AppEntryBytes::try_from(result.map_err(|e| wasm_error!(e.into()))?).map_err(|entry_error| match entry_error {
-    //                 EntryError::SerializedBytes(serialized_bytes_error) => {
-    //                     wasm_error!(WasmErrorInner::Serialize(serialized_bytes_error))
-    //                 }
-    //                 EntryError::EntryTooLarge(_) => {
-    //                     wasm_error!(WasmErrorInner::Guest(entry_error.to_string()))
-    //                 }
-    //             })
-    //         }
-    //     }
-    //     impl TryFrom<#ident> for AppEntryBytes {
-    //         type Error = WasmError;
-    //         fn try_from(t: #ident) -> Result<Self, Self::Error> {
-    //             Self::try_from(&t)
-    //         }
-    //     }
-
-    //     impl TryFrom<&#ident> for Entry {
-    //         type Error = WasmError;
-    //         fn try_from(t: &#ident) -> Result<Self, Self::Error> {
-    //             Ok(Self::App(AppEntryBytes::try_from(t)?))
-    //         }
-    //     }
-
-    //     impl TryFrom<#ident> for Entry {
-    //         type Error = WasmError;
-    //         fn try_from(t: #ident) -> Result<Self, Self::Error> {
-    //             Self::try_from(&t)
-    //         }
-    //     }
-
-    // };
-    // output.into()
 }
